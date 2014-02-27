f516c781dcd1e4ec546984bb3ba5493a583e6c75
Chenfei Wang <wcftongji@gmail.com>
2014-02-27 15:29:21 -0500

    remove cut bias

--- gcap/funcs/union_dhs_overlap.py
+++ gcap/funcs/union_dhs_overlap.py
@@ -29,115 +29,115 @@ def union_DHS_overlap(workflow, conf, tex):
                 output = output,
                 name = "Write out DHS overlap BED"))
 
-        if conf.get("cut_bias", "run").strip().upper() == "T":
-            ## overall mode with make_bg_bwDHS_twobit.py
-            if conf.get("cut_bias", "mode") == "overall":
-                attach_back(workflow,
-                            ShellCommand(
-                                """
-                                {tool} bamtobed -i {input[tag]} > {output[tags]}
-                                awk '{{if ($6=="+") print $0}}' {output[tags]} > {output[plus_tag]}
-                                awk '{{if ($6=="-") print $0}}' {output[tags]} > {output[minus_tag]}
-                                macs2 pileup -i {output[plus_tag]} -o {output[plus_bdg]} -f BED --extsize 1
-                                macs2 pileup -i {output[minus_tag]} -o {output[minus_bdg]} -f BED --extsize 1
-                                bedClip {output[plus_bdg]} {param[chr]} {output[plus_bdg]}.clip
-                                bedGraphToBigWig -unc {output[plus_bdg]}.clip {param[chr]} {output[bigwig_plus]}
-                                bedClip {output[minus_bdg]} {param[chr]} {output[minus_bdg]}.clip
-                                bedGraphToBigWig -unc {output[minus_bdg]}.clip {param[chr]} {output[bigwig_minus]}
-                                rm -f {output[plus_bdg]}.clip
-                                rm -f {output[minus_bdg]}.clip
-                                """,
-                                tool = "bedtools",
-                                input = {"tag": target + ".bam"},
-                                output = {"tags": target + "_cut_input.bed", "bigwig_plus": target + "_plus.bw",
-                                          "bigwig_minus": target + "_minus.bw",
-                                          "plus_tag": target + "cut_plus.bed", "minus_tag": target + "cut_minus.bed",
-                                          "plus_bdg": target + "plus.bdg", "minus_bdg": target + "minus.bdg"},
-                                name = " extract 1bp pileup from bam ",
-                                param = {"chr": conf.get("lib", "chrom_len")}))
-
-
-                attach_back(workflow,
-                            ShellCommand(
-                                """
-                                ## use whole genome for atac seq
-                                {tool} {param[script]} -p {input[bigwig_plus]} -n {input[bigwig_minus]} --genome={param[genome]} --sc {param[nmer_cut]} \
-                                --left={param[mer]} --right={param[mer]} -o {output[matrix]} -i {input[peaks]}""",
-                                tool = "python2.7",
-                                input = {"bigwig_minus": target + "_minus.bw", "bigwig_plus": target + "_plus.bw",
-                                         "peaks": conf.get("lib", "dhs")},
-                                output = {"matrix": target + "_cut_bias.xls"},
-                                param = {"script": resource_filename("gcap", "pipeline-scripts/make_bg_bwDHS_twobit.py"),
-                                         "genome": conf.get("cut_bias", "seq_2bit"), ## "peaks": conf.get("lib", "dhs"),
-                                         "nmer_cut": 10,
-                                         "mer": 3}))
-
-                ## Shawn's version of footprint
-                ### 1. overlap with your DHS site , bedtools required
-                attach_back(workflow,
-                        ShellCommand(
-                            """
-                            ## input is chip seq peaks, openchr is dnase, atac or other open chromatin techniques peaks
-                            ## support for macs2 peaks calling mode only
-                            cut -f 1,2,3,4,9 {input[dhs]} > {input[dhs]}.bed
-                            intersectBed -a {input[motif]} -b {input[dhs]}.bed -wa -u > {output[motif_dhs]} """,
-                            tool = "intersectBed",
-                            input = {"motif": conf.get("footprint", "motif"),
-                                     "dhs": target + "_macs2_all_peaks.encodePeak"},
-                            output = {"motif_dhs": target + "_motif_dhs.bed"}))
-
-                ### 2. scan for plotting data use python script, Scan_6c_matrix_cv_chip.py
-                attach_back(workflow,
-                        ShellCommand(
-                            """
-                            {tool} {param[script]} -i {input[motif_bed]} -o {output[matrix]} -b {input[cut_bias]} -p {input[bigwig_plus]} -n {input[bigwig_minus]} -c {input[conservation]} --chipbw {input[chip]}
-                            """,
-                            tool = "python2.7",
-                            input = {"bigwig_plus": target + "_plus.bw",
-                                     "bigwig_minus": target + "_minus.bw",
-                                     "cut_bias": target + "_cut_bias.xls",
-                                     "chip": conf.get("footprint", "chip"),
-                                     "motif_bed": target + "_motif_dhs.bed", "conservation": conf.get("footprint", "conservation")},
-                            output = {"matrix": target + "_footprint.xls"},
-                            param = {"script": "Scan_6c_matrix_cv_chip.py"}))
-
-                attach_back(workflow,
-                    ShellCommand(
-                        """
-                        Rscript plot6c_cv_chip_10k.r {input[matrix]} {output}
-                        """,
-                        tool = "Rscript",
-                        input = {"matrix": target + "_footprint.xls"},
-                        output = target + "_footprint")) 
-                ## pyDNase involved in footprint detection
-                ## pyDNase
-                ## import pyDNase
-
-            ## strand mode for cutting bias
-            ## cutting bias in stranded mode, a bit strange for ATAC-seq
-            if conf.get("cut_bias", "mode") == "strand":
-                if "bed" in conf.seq_type:
-                    cut = attach_back(workflow,
-                        ShellCommand(
-                            "{tool} {param[script]} -p {input[peaks]} -s {input[bit]} -o {output[matrix]} -f {param[mer]} -t {input[tag]}",
-                            tool = "python2.7",
-                            input = {"peaks": conf.get("lib", "dhs"), "bit": conf.get("cut_bias", "seq_2bit"),
-                                     "tag": target + "_all.bed"},
-                            output = {"matrix": conf.treatment_targets[i] + "_cut_bias.xls"},
-                            param = {"mer": 3, "script": resource_filename("gcap", "pipeline-scripts/twobit_seqbias.py")},
-                            name = "cutting bias"))
-                    cut.update(param = conf.items("cut_bias"))
-                else:
-                    cut = attach_back(workflow,
-                        ShellCommand(
-                            "bamToBed -i {input[tag]} > {output[tag]} && {tool} {param[script]} -p {input[peaks]} -s {input[bit]} -o {output[matrix]} -f {param[mer]} -t {output[tag]}",
-                            tool = "python2.7",
-                            input = {"peaks": conf.get("lib", "dhs"), "bit": conf.get("cut_bias", "seq_2bit"),
-                                     "tag": target + ".bam"},
-                            output = {"tag": target + "_cut_input.bed", "matrix": conf.treatment_targets[i] + "_cut_bias.xls"},
-                            param = {"mer": 3, "script": resource_filename("gcap", "pipeline-scripts/twobit_seqbias.py")},
-                            name = "cutting bias"))
-                    cut.update(param = conf.items("cut_bias"))
+        # if conf.get("cut_bias", "run").strip().upper() == "T":
+        #     ## overall mode with make_bg_bwDHS_twobit.py
+        #     if conf.get("cut_bias", "mode") == "overall":
+        #         attach_back(workflow,
+        #                     ShellCommand(
+        #                         """
+        #                         {tool} bamtobed -i {input[tag]} > {output[tags]}
+        #                         awk '{{if ($6=="+") print $0}}' {output[tags]} > {output[plus_tag]}
+        #                         awk '{{if ($6=="-") print $0}}' {output[tags]} > {output[minus_tag]}
+        #                         macs2 pileup -i {output[plus_tag]} -o {output[plus_bdg]} -f BED --extsize 1
+        #                         macs2 pileup -i {output[minus_tag]} -o {output[minus_bdg]} -f BED --extsize 1
+        #                         bedClip {output[plus_bdg]} {param[chr]} {output[plus_bdg]}.clip
+        #                         bedGraphToBigWig -unc {output[plus_bdg]}.clip {param[chr]} {output[bigwig_plus]}
+        #                         bedClip {output[minus_bdg]} {param[chr]} {output[minus_bdg]}.clip
+        #                         bedGraphToBigWig -unc {output[minus_bdg]}.clip {param[chr]} {output[bigwig_minus]}
+        #                         rm -f {output[plus_bdg]}.clip
+        #                         rm -f {output[minus_bdg]}.clip
+        #                         """,
+        #                         tool = "bedtools",
+        #                         input = {"tag": target + ".bam"},
+        #                         output = {"tags": target + "_cut_input.bed", "bigwig_plus": target + "_plus.bw",
+        #                                   "bigwig_minus": target + "_minus.bw",
+        #                                   "plus_tag": target + "cut_plus.bed", "minus_tag": target + "cut_minus.bed",
+        #                                   "plus_bdg": target + "plus.bdg", "minus_bdg": target + "minus.bdg"},
+        #                         name = " extract 1bp pileup from bam ",
+        #                         param = {"chr": conf.get("lib", "chrom_len")}))
+        #
+        #
+        #         attach_back(workflow,
+        #                     ShellCommand(
+        #                         """
+        #                         ## use whole genome for atac seq
+        #                         {tool} {param[script]} -p {input[bigwig_plus]} -n {input[bigwig_minus]} --genome={param[genome]} --sc {param[nmer_cut]} \
+        #                         --left={param[mer]} --right={param[mer]} -o {output[matrix]} -i {input[peaks]}""",
+        #                         tool = "python2.7",
+        #                         input = {"bigwig_minus": target + "_minus.bw", "bigwig_plus": target + "_plus.bw",
+        #                                  "peaks": conf.get("lib", "dhs")},
+        #                         output = {"matrix": target + "_cut_bias.xls"},
+        #                         param = {"script": resource_filename("gcap", "pipeline-scripts/make_bg_bwDHS_twobit.py"),
+        #                                  "genome": conf.get("cut_bias", "seq_2bit"), ## "peaks": conf.get("lib", "dhs"),
+        #                                  "nmer_cut": 10,
+        #                                  "mer": 3}))
+        #
+        #         ## Shawn's version of footprint
+        #         ### 1. overlap with your DHS site , bedtools required
+        #         attach_back(workflow,
+        #                 ShellCommand(
+        #                     """
+        #                     ## input is chip seq peaks, openchr is dnase, atac or other open chromatin techniques peaks
+        #                     ## support for macs2 peaks calling mode only
+        #                     cut -f 1,2,3,4,9 {input[dhs]} > {input[dhs]}.bed
+        #                     intersectBed -a {input[motif]} -b {input[dhs]}.bed -wa -u > {output[motif_dhs]} """,
+        #                     tool = "intersectBed",
+        #                     input = {"motif": conf.get("footprint", "motif"),
+        #                              "dhs": target + "_macs2_all_peaks.encodePeak"},
+        #                     output = {"motif_dhs": target + "_motif_dhs.bed"}))
+        #
+        #         ### 2. scan for plotting data use python script, Scan_6c_matrix_cv_chip.py
+        #         attach_back(workflow,
+        #                 ShellCommand(
+        #                     """
+        #                     {tool} {param[script]} -i {input[motif_bed]} -o {output[matrix]} -b {input[cut_bias]} -p {input[bigwig_plus]} -n {input[bigwig_minus]} -c {input[conservation]} --chipbw {input[chip]}
+        #                     """,
+        #                     tool = "python2.7",
+        #                     input = {"bigwig_plus": target + "_plus.bw",
+        #                              "bigwig_minus": target + "_minus.bw",
+        #                              "cut_bias": target + "_cut_bias.xls",
+        #                              "chip": conf.get("footprint", "chip"),
+        #                              "motif_bed": target + "_motif_dhs.bed", "conservation": conf.get("footprint", "conservation")},
+        #                     output = {"matrix": target + "_footprint.xls"},
+        #                     param = {"script": "Scan_6c_matrix_cv_chip.py"}))
+        #
+        #         attach_back(workflow,
+        #             ShellCommand(
+        #                 """
+        #                 Rscript plot6c_cv_chip_10k.r {input[matrix]} {output}
+        #                 """,
+        #                 tool = "Rscript",
+        #                 input = {"matrix": target + "_footprint.xls"},
+        #                 output = target + "_footprint"))
+        #         ## pyDNase involved in footprint detection
+        #         ## pyDNase
+        #         ## import pyDNase
+        #
+        #     ## strand mode for cutting bias
+        #     ## cutting bias in stranded mode, a bit strange for ATAC-seq
+        #     if conf.get("cut_bias", "mode") == "strand":
+        #         if "bed" in conf.seq_type:
+        #             cut = attach_back(workflow,
+        #                 ShellCommand(
+        #                     "{tool} {param[script]} -p {input[peaks]} -s {input[bit]} -o {output[matrix]} -f {param[mer]} -t {input[tag]}",
+        #                     tool = "python2.7",
+        #                     input = {"peaks": conf.get("lib", "dhs"), "bit": conf.get("cut_bias", "seq_2bit"),
+        #                              "tag": target + "_all.bed"},
+        #                     output = {"matrix": conf.treatment_targets[i] + "_cut_bias.xls"},
+        #                     param = {"mer": 3, "script": resource_filename("gcap", "pipeline-scripts/twobit_seqbias.py")},
+        #                     name = "cutting bias"))
+        #             cut.update(param = conf.items("cut_bias"))
+        #         else:
+        #             cut = attach_back(workflow,
+        #                 ShellCommand(
+        #                     "bamToBed -i {input[tag]} > {output[tag]} && {tool} {param[script]} -p {input[peaks]} -s {input[bit]} -o {output[matrix]} -f {param[mer]} -t {output[tag]}",
+        #                     tool = "python2.7",
+        #                     input = {"peaks": conf.get("lib", "dhs"), "bit": conf.get("cut_bias", "seq_2bit"),
+        #                              "tag": target + ".bam"},
+        #                     output = {"tag": target + "_cut_input.bed", "matrix": conf.treatment_targets[i] + "_cut_bias.xls"},
+        #                     param = {"mer": 3, "script": resource_filename("gcap", "pipeline-scripts/twobit_seqbias.py")},
+        #                     name = "cutting bias"))
+        #             cut.update(param = conf.items("cut_bias"))
 
 
     attach_back(workflow, PythonCommand(
